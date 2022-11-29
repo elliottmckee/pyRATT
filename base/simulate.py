9120649882cc5bc66b6b99ab2142d9141266239a
elliottmckee <elliott.mckee@colorado.edu>
2022-11-29 15:48:03 -0700

    Absurdly high-level/coarse organization outline

---
+++ base/simulate.py
@@ -0,0 +1,34 @@
+
+
+
+#This will be the main driver script performing the simulation, handling which modules are input/ran, etc.
+
+
+
+#import needed modules
+
+
+# read in trajectory/rocket file
+
+
+
+#initialize simulation
+
+
+
+#perform simulation
+
+
+
+
+#post-processing
+
+
+
+#saving, data export
+
+
+
+
+
+
---
+++ stata_mater.py
@@ -0,0 +1,60 @@
+'''
+
+I will be coming back to this later to expand this header a good bit
+
+
+Description:
+This will be the main script that the User interacts with. 
+They will select which modules to run, point to the desired
+rocket/trajectory files, maybe point to a config file 
+containing extra information, and this will hand things off to simulate.py
+
+
+'''
+
+
+# import __________ 
+
+
+
+
+#########################################
+########    MODULES TO RUN      #########
+#########################################
+#Boolean- if you want to run a specific module, you put a 1 next to it
+
+transient_thermal               = 1
+
+environment_characterization    = 0
+lumped_capacitance              = 0
+
+
+
+#########################################
+########    USER FILE SPECIFICATION      #########
+#########################################
+
+#RAS_file = 'meatrocket.CDX1'
+#trajectory_file = 'meatrocket_nominal.csv'
+#output_filename = 'meat_out.idkyet'
+
+#addtl_geometry_specification = 'meatrocket_nosecone.py'
+
+
+
+
+########################################################################
+####### END OF INPUTS FIELD
+########################################################################
+
+#Run Simuation
+#simulate(ASDFAHIASVIARIVJAR)
+
+
+
+
+
+
+
+
+
