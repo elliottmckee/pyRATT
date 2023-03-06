c117a96538ddd7709972fc2856214c55e2d0652d
elliottmckee <elliott.mckee@colorado.edu>
2023-03-05 19:06:09 -0500

    Adding a bunch of useless shit im gonna have to delete

--- stata_mater/animation_dev/animation_dev.py
+++ stata_mater/animation_dev/animation_dev.py
@@ -1,9 +1,9 @@
 '''
 ---------------------------------------------------------------------------
-PyRATT - Python Rocket AeroThermal Toolbox 
+pyRATT - Python Rocket AeroThermal Toolbox 
 ---------------------------------------------------------------------------
 
-PyRATT is a thermal analysis toolbox for *very* high-performance/high-altitude 
+pyRATT is a thermal analysis toolbox for *very* high-performance/high-altitude 
 amateur rocketry applications. 
 
 Primarily, it can perform a 1-Dimensional, transient thermal simulation of 
@@ -100,7 +100,6 @@ NOTES:
 
 
 TODO : 
-- Fix Bug with Coordinates when using multi-component walls
 - Add additional Hifire heatflux comparison plots
 - Add Thermal Interface Resistances
 - Ablation Modelling
@@ -142,31 +141,116 @@ from src.materials_gas import AirModel
 import src.tools_postproc as Post
 
 
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+#plt.style.use('seaborn-pastel')
+
+
 
 if __name__ == "__main__":
 
-
-
-    # Define Wall
-    AeroSurf = WallStack(materials="ALU6061", thicknesses=0.02, node_counts = 15)
-
-    # Point to Trajectory Data CSV
-    Flight    = FlightProfile( os.path.join(os.getcwd(), "example_files", "example_ascent_traj_M2245_to_M1378.csv") )
     
-    # Define Simulation Object
-    MySimulation= Thermal_Sim_1D(AeroSurf, Flight, AirModel(),
-                                x_location = 0.2, 
-                                deflection_angle_deg = 7.0, 
-                                t_end = 30.0,
-                                t_step = 0.0050,
-                                boundary_layer_model = 'transition')
+    # Read in Sim File
+    with open("multi_material_example_out.sim", "rb") as input_file:
+            Sim = pickle.load(input_file)
 
-    #Run Simulation
-    MySimulation.run()
+        
+
+    # print(Sim.y_coords)
+
+    # quit()
 
 
-    #Export to CSV
-    MySimulation.export_data_to_csv()
+    plt.rcParams['text.color'] = "#f8f8f2"
+    plt.rcParams['axes.labelcolor'] = "#f8f8f2"
+    plt.rcParams['xtick.color'] = "#f8f8f2"
+    plt.rcParams['ytick.color'] = "#f8f8f2"
 
-    #Export via Pickle
-    with open("animtest_2material.sim", "wb") as f: pickle.dump(MySimulation, f)
+    #Create Figure
+    fig, ax = plt.subplots(2, facecolor="#282a36", figsize=(13, 10))
+    #plt.tight_layout()
+
+    
+
+    # Plot 1 
+    line, = ax[0].plot(Sim.y_coords, Sim.wall_temps[:,0], color="#bd93f9", linewidth=2)
+
+    ax[0].set_title(f"Wall Temperature Distribution at t=0.000 seconds")
+    ax[0].set_xlim(min(Sim.y_coords), max(Sim.y_coords))
+    ax[0].set_ylim( 0.95*np.amin(Sim.wall_temps), 1.05*np.amax(Sim.wall_temps))
+
+    ax[0].set_xlabel("Through-Wall Location [m]")
+    ax[0].set_ylabel("Temperature [K]", color="#bd93f9")
+    ax[0].tick_params(axis='y', labelcolor="#bd93f9")
+    ax[0].set_facecolor("#44475a")
+
+    ax[0].grid(True, color="#282a36")
+
+    verticalline2, = ax[0].plot([0.00666, 0.00666], [-1000.0, 1000000.0], "k--", linewidth=2)
+
+    ax[0].annotate("<- Carbon Fiber", [0.0054,800], color="#ff79c6")
+    ax[0].annotate("Aluminum ->",  [0.0068, 800], color="#ff79c6")
+
+
+    # verticalline2, = ax[0].plot([0.00635, 0.00635], [-1000.0, 1000000.0], "k--", linewidth=2)
+    # ax[0].annotate("Fin Centerline", [0.00640, 530], color="#000000")
+    # ax[0].set_xticks([0.0, 0.003175, 0.00635, 0.009525, 0.0127])
+    
+
+
+
+    # Plot 2 
+    ax2 = ax[1].twinx()
+
+    ax[1].set_title("Flight Profile")
+    ax[1].set_xlim([0.0, max(Sim.t_vec)])
+    ax[1].set_xlabel("Flight Time (s)")
+    ax[1].set_ylabel('Mach', color="#50fa7b")
+    ax[1].tick_params(axis='y', labelcolor="#50fa7b")
+    ax[1].set_facecolor("#44475a")
+    ax[1].grid(True, color="#282a36")
+
+    
+    
+    
+    
+    machline, = ax[1].plot(Sim.t_vec, Sim.mach, color="#50fa7b")
+    machpoint = ax[1].scatter(Sim.t_vec[0], Sim.mach[0], color="#50fa7b")
+
+
+    ax2.set_ylim([0.0, max(Sim.alt)])
+    ax2.set_ylabel('Altitude [m]', color="#8be9fd")
+    ax2.tick_params(axis='y', labelcolor="#8be9fd")
+    altline, = ax2.plot(Sim.t_vec, Sim.alt, color="#8be9fd")
+    altpoint = ax2.scatter(Sim.t_vec[0], Sim.alt[0], color="#8be9fd")
+
+    verticalline, = ax2.plot([0.0, 0.0], [-1000.0, 1000000.0], "k--", linewidth=0.5)
+
+    def animate(i):
+        
+        #Skipping timesteps since i is called sequentialls
+        i = i*40
+
+
+        ax[0].set_title(f"Wall Temperature Distribution at t={Sim.t_vec[i]:.3f} seconds")
+
+        line.set_ydata(np.flip(Sim.wall_temps[:,i]))
+
+        verticalline.set_xdata([Sim.t_vec[i],Sim.t_vec[i]])
+
+        machpoint.set_offsets([Sim.t_vec[i], Sim.mach[i]])
+        altpoint.set_offsets([Sim.t_vec[i], Sim.alt[i]])
+
+
+        return line
+
+    ani = animation.FuncAnimation(fig, animate, frames=250, interval= 50, repeat=True) 
+
+    #ani.save('flight.gif', fps=10.0)
+
+
+    ani.save("test.gif", writer= animation.PillowWriter(fps=20))
+    
+    
+    #plt.show()
